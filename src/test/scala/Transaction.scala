6b5f3d1d508b10800c5bfbb40c0aa8d5b728e6dc
Giuseppe Cannella <giuseppe.cannella@gmail.com>
2019-07-29 16:03:07 +0200

    transaction

--- src/test/scala/Transaction.scala
+++ src/test/scala/Transaction.scala
@@ -1,9 +1,14 @@
+import cats.effect.{IO, Resource}
+import cats.free.Free
+import doobie.Fragments
 import doobie.enum.SqlState
+import doobie.free.connection
+import doobie.hikari.HikariTransactor
 import org.scalatest.FunSuite
 
 class Transaction extends FunSuite {
 
-  test("transaction") {
+  test("transaction ko") {
     import MyPredef.transactor
     import doobie.implicits._
 
@@ -15,34 +20,82 @@ class Transaction extends FunSuite {
       sql"insert into city (id, name, countrycode,district,population) values ($id, $name, $countrycode,$district,$population)".update
 
     //duplicate key value violates unique constraint "city_pkey". No records insert!
-    val o = transactor
+    val oo: Resource[IO, HikariTransactor[IO]] = transactor
+
+    val randKey = scala.util.Random.nextInt()
+    val rows: Free[connection.ConnectionOp, String] = for {
+      _ <- insertCity(randKey, "city1", "c1", "d1", 10).run
+      _ <- insertCity(randKey, "city1", "c1", "d1", 10).run
+    } yield "inserted"
+
+    val res = oo
       .use { xa =>
-        for {
-          _ <- insertCity(5000, "city1", "c1", "d1", 10).run.transact(xa).attemptSomeSqlState {
-            case a => a
-          }
-          b <- insertCity(5000, "city1", "c1", "d1", 10).run.transact(xa).attemptSomeSqlState {
-            case a => a
-          }
-        } yield b
+        rows.transact(xa).handleErrorWith { e =>
+          IO(e.getMessage)
+        }
       }
       .unsafeRunSync()
 
-    o match {
-      case Right(_) => assert(false)
-      case Left(l)  => assert(l == SqlState("23505")) //unique constraint
-    }
+    assert(res == s"""ERROR: duplicate key value violates unique constraint "city_pkey"
+                    |  Detail: Key (id)=($randKey) already exists.""".stripMargin)
 
     //read - no records found
     {
-      val mySelect = transactor.use { xa =>
-        sql"select id from person where id = 5000 "
-          .query[Int]
-          .to[List]
-          .transact(xa)
-      }.unsafeRunSync
-
+      val mySelect = transactor
+        .use { xa =>
+          fr"""select id from city where id = $randKey"""
+            .query[Int]
+            .to[List]
+            .transact(xa)
+        }
+        .unsafeRunSync()
       assert(mySelect.isEmpty)
+    }
+
+  }
+
+  test("transaction ok") {
+    import MyPredef.transactor
+    import doobie.implicits._
+
+    case class City(id: Long, name: String, countrycode: String, district: String, population: Int)
+
+    import doobie.util.update.Update0
+
+    def insertCity(id: Long, name: String, countrycode: String, district: String, population: Int): Update0 =
+      sql"insert into city (id, name, countrycode,district,population) values ($id, $name, $countrycode,$district,$population)".update
+
+    //duplicate key value violates unique constraint "city_pkey". No records insert!
+    val oo: Resource[IO, HikariTransactor[IO]] = transactor
+
+    val randKey1 = scala.util.Random.nextInt()
+    val randKey2 = scala.util.Random.nextInt()
+    val rows: Free[connection.ConnectionOp, String] = for {
+      _ <- insertCity(randKey1, "city1", "c1", "d1", 10).run
+      _ <- insertCity(randKey2, "city1", "c1", "d1", 10).run
+    } yield "inserted"
+
+    val res = oo
+      .use { xa =>
+        rows.transact(xa).handleErrorWith { e =>
+          IO(e.getMessage)
+        }
+      }
+      .unsafeRunSync()
+
+    assert(res == """inserted""".stripMargin)
+
+    //read - no records found
+    {
+      val mySelect = transactor
+        .use { xa =>
+          fr"""select id from city where id in( $randKey1, $randKey2)""".stripMargin
+            .query[Int]
+            .to[List]
+            .transact(xa)
+        }
+        .unsafeRunSync()
+      assert(mySelect.size == 2)
     }
 
   }
